5c067aaeda3b503dc7f3757e850e213043dfdbdc
kenji0033 <134219989+kenji0033@users.noreply.github.com>
2026-02-01 03:03:51 +0100

    thot

---
+++ Assets/GameJam/Scripts/Enemy/Thot.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using UnityEngine;
+
+public class Thot : Enemy
+{
+    [SerializeField] protected GameObject bulletprefab;
+    
+    [SerializeField] protected float speed;
+
+
+    // Cambiamos esto para que sea un contador simple
+    [SerializeField] protected float specialCoolDownDuration = 30f;
+    private float _specialTimer;
+
+    private void Start()
+    {
+        // Inicializamos el timer para que el primer ataque especial tarde 30s
+        _specialTimer = specialCoolDownDuration;
+
+        // Si usas NavMeshAgent, recuerda desactivar la rotación automática
+        // _agent.updateRotation = false; 
+    }
+
+    private void Update()
+    {
+
+        // 2. Lógica del Ataque Normal (Raycast)
+        if (shotCoolDown <= 0)
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, attackrange, playerLayer))
+            {
+                Attack();
+                shotCoolDown = startShotCoolDown;
+            }
+        }
+        shotCoolDown -= Time.deltaTime;
+
+        // 3. Lógica del Ataque Especial (Temporizador simple)
+        _specialTimer -= Time.deltaTime;
+        if (_specialTimer <= 0)
+        {
+            SpecialAttack();
+            _specialTimer = specialCoolDownDuration; // Reinicia el ciclo de 30s
+        }
+    }
+
+    protected override void Attack()
+    {
+        var bullet = Instantiate(bulletprefab, attackpoint.position, attackpoint.rotation);
+        bullet.GetComponent<Rigidbody>().linearVelocity = attackpoint.forward * speed;
+        Debug.Log("Disparo normal");
+    }
+
+    protected void SpecialAttack()
+    {
+        Debug.Log("cura");
+    }
+}
